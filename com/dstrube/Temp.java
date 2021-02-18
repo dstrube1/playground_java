413293948c4e004427efff57705ba2e2cd724ae2
David Strube <dstrube@gmail.com>
2021-02-18 14:51:42 -0500

    Create Temp.java

---
+++ com/dstrube/Temp.java
@@ -0,0 +1,42 @@
+package com.dstrube;
+
+/*
+From ~/java:
+
+Mac:
+javac -d bin com/dstrube/Temp.java
+java -cp bin com.dstrube.Temp
+
+Windows:
+javac -d bin com\dstrube\Temp.java
+java -cp bin com.dstrube.Temp
+
+This is a temporary code holder.
+Nothing here is permanent. 
+Must be okay with losing anything in here; else, save it to somewhere else.
+*/
+
+import java.util.*;
+import java.math.BigInteger;
+import java.io.*;
+
+public class Temp {
+	public static void main(String[] args) {
+		int i;
+		long L;
+		byte b;
+		char c;
+		String s;
+		short sh;
+		float f;
+		double d;
+		List<Integer> list = new ArrayList<>();
+		Queue<Byte> queue = new ArrayDeque<>();
+		Stack<Short> stack = new Stack<>();
+		BigInteger bi = new BigInteger("1");
+		StringBuilder sb = new StringBuilder();
+	}
+	
+	public static void x(String[] args) {
+	}
+}
