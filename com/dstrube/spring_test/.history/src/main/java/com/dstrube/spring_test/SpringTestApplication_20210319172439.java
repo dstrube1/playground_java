cfef79d47d6d1b757155149c8262ad0a9d271396
David Strube <79855409+dstrubeCCM@users.noreply.github.com>
2021-03-19 17:25:51 -0400

    added spring test application

---
+++ com/dstrube/spring_test/.history/src/main/java/com/dstrube/spring_test/SpringTestApplication_20210319172439.java
@@ -0,0 +1,15 @@
+package com.dstrube.spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// import javax.inject
+
+@SpringBootApplication
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+
+}
