87b6f24644b578b1701ca1316d65ad0220fe1175
David Strube <dstrube3@gatech.edu>
2025-10-07 07:54:27 -0400

    Update Sieve.java

--- com/dstrube/Sieve.java
+++ com/dstrube/Sieve.java
@@ -18,18 +18,39 @@ public class Sieve {
 	}
 	
 	public static void runEratosthenesSieve(int upperBound) {
-      int upperBoundSquareRoot = (int) Math.sqrt(upperBound);
-      boolean[] isComposite = new boolean[upperBound + 1];
-      for (int m = 2; m <= upperBoundSquareRoot; m++) {
-            if (!isComposite[m]) {
-                  System.out.print(m + " ");
-                  for (int k = m * m; k <= upperBound; k += m)
-                        isComposite[k] = true;
-            }
-      }
+		int upperBoundSquareRoot = (int) Math.sqrt(upperBound);
+		boolean[] isComposite = new boolean[upperBound + 1];
+		int dotCount = 0;
+		int hashCount = 0;
+		for (int m = 2; m <= upperBoundSquareRoot; m++) {
+			if (!isComposite[m]) {
+				//System.out.print(m + " ");
+				dotCount++;
+				if(dotCount % 100 == 0)
+					System.out.print(". ");
+				for (int k = m * m; k <= upperBound; k += m)
+					isComposite[k] = true;
+			}
+		}
+		System.out.println("dot count: " + dotCount);
 
-      for (int m = upperBoundSquareRoot; m <= upperBound; m++)
-            if (!isComposite[m])
-                  System.out.print(m + " ");
+		//int debugPrintLimit = 0;
+
+		for (int m = upperBoundSquareRoot; m <= upperBound; m++){
+			/*if(debugPrintLimit < 100) {
+				debugPrintLimit++;
+				System.out.println("m = " + m + " ");
+				System.out.println("upperBound = " + upperBound);
+			}*/
+			if (!isComposite[m]){
+				//System.out.print(m + " ");
+				hashCount++;
+				if(hashCount % 100000 == 0){
+					System.out.print("# ");
+					
+				}
+			}
+		}
+		//System.out.println("hash count: " + hashCount);
 	}
 }
