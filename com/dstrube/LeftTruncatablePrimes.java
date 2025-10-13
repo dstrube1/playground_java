13194bc6d9db961a33bc0efcd24b74d12b44550b
David Strube <dstrube3@gatech.edu>
2025-10-13 11:08:40 -0400

    Update LeftTruncatablePrimes.java

--- com/dstrube/LeftTruncatablePrimes.java
+++ com/dstrube/LeftTruncatablePrimes.java
@@ -7,8 +7,16 @@ javac -d bin com/dstrube/LeftTruncatablePrimes.java
 java -cp bin com.dstrube.LeftTruncatablePrimes
 
 */
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
 //https://docs.oracle.com/javase/8/docs/api/java/math/BigInteger.html
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class LeftTruncatablePrimes{
 	
@@ -20,7 +28,28 @@ public class LeftTruncatablePrimes{
 		boolean is = largestLeft.isProbablePrime(1);
 		System.out.println("Is " + largestLeft + " probably prime? " + is);
 		
-		//TODO: Prove it with brute force
+		//TODO: Prove it, not with isProbablePrime, but with the Sieve of Eratosthenes
+		// On second thought, can Sieve of Eratosthenes work here? It's dependent on an array of booleans, 
+		// which is limited by Integer.MAX_VALUE; it can output a large number of primes,
+		// but is its largest big enough for this? TBD by Sieve.java...
+		// In the mean time, prepare to read from file:
+		final String fileName = "primes.txt";
+		File file = new File(fileName);
+		if (file.exists()){
+			System.out.println("File " + fileName + " found. Getting the contents...");
+			Path path = Path.of(fileName);
+			try{
+				List<Integer> primes = readFile(path);
+				System.out.println("Size of contents: " + primes.size());
+			} catch(IOException ioe){
+				System.out.println("Caught: " + ioe.getMessage());
+			}
+		}else{
+			//create the file?
+			//or tell the user to run Sieve to create the file?
+			System.out.println("File " + fileName + " not found...");
+			//return;
+		}
 		
 		//Why use any certainty other than 1?
 		//https://stackoverflow.com/questions/27430092/what-is-a-possible-use-case-of-bigintegers-isprobableprime
@@ -47,6 +76,22 @@ public class LeftTruncatablePrimes{
 		BigInteger largestLeftRight = new BigInteger("739397");
 		
 		//Not todo: prove that any-deleteable primes are infinite :-p
+		
+		System.out.println("Done");
 	}
+	
+	private static List<Integer> readFile(Path path) throws IOException {
+			//Path path = Path.of("");
+			//String content = readFile(path);
+			//System.out.println("Content: " + content);
+		//StringBuilder data = new StringBuilder();
+	    Stream<String> lines = Files.lines(path);
+	    List<Integer> primes = new ArrayList<>();
+	    lines.forEach(line -> primes.add(Integer.parseInt(line)));
+	    lines.close();
+	    
+	    return primes;
+	}
+	
 		
 }
