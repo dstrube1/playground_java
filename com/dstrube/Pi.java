d82c09051439947bcf76cd26673b1d9467a94c4e
David Strube <dstrube3@gatech.edu>
2025-10-28 08:33:54 -0400

    Considalting PiExplorer into the older Pi.java

--- com/dstrube/Pi.java
+++ com/dstrube/Pi.java
@@ -5,7 +5,15 @@ compile:
 javac -d bin com/dstrube/Pi.java
 run:
 java -cp bin com.dstrube.Pi
+
+From:
+https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
+
+https://docs.oracle.com/javase/8/docs/api/java/math/BigDecimal.html
+
 */
+
+import java.math.BigDecimal;
 
 public class Pi{
 	private static float piF = 4.0f;
@@ -15,12 +23,59 @@ public class Pi{
 	private static long count;
 	
 	public static void main(String[] args){
-//		System.out.println("Pi: " + pi);
-//		System.out.println("Float.SIZE = " + Float.SIZE);		
-//		System.out.println("Float.MIN_VALUE = " + Float.MIN_VALUE);
-//		System.out.println("Float.MIN_NORMAL = " + Float.MIN_NORMAL);
-//		System.out.println("Float.MIN_EXPONENT = " + Float.MIN_EXPONENT);
+		System.out.println("Math.Pi: " + Math.PI);							// 3.141592653589793
+		//System.out.println("Float.SIZE = " + Float.SIZE);					// 32
+		//System.out.println("Float.MIN_VALUE = " + Float.MIN_VALUE);		// 1.4E-45
+		//System.out.println("Float.MIN_NORMAL = " + Float.MIN_NORMAL);		// 1.1754944E-38
+		//System.out.println("Float.MIN_EXPONENT = " + Float.MIN_EXPONENT); // -126
 		
+		final int MILLION =  1_000_000;
+		final int TMILLION = 10_000_000; //billion and HMillion don't work right
+		double extraDigits = Math.PI * MILLION;
+		//System.out.println("pi * million: " + extraDigits);
+
+		extraDigits %= 1;
+		extraDigits *= TMILLION;
+		System.out.println("extra digits:   " + ("" + extraDigits).replace(".",""));	
+		// 3.141592653589793
+		// 		   6535897930152714
+		
+		/////////////////////////////////////////////////////////////////////////////////
+		// To illustrate "not right" :
+		final int HMILLION = 100_000_000; 
+		extraDigits = Math.PI * MILLION;
+		extraDigits %= 1;
+		extraDigits *= HMILLION;
+		//System.out.println("extra digits done badly (pt 1 of 2):   " + extraDigits);
+		// 6.535897930152714E7
+		
+		final int BILLION =  1_000_000_000; 
+		extraDigits = Math.PI * MILLION;
+		extraDigits %= 1;
+		extraDigits *= BILLION;
+		//System.out.println("extra digits done badly (pt 2 of 2):   " + extraDigits);
+		// 6.535897930152714E8
+		
+		//What if step 1 = HMILLION and step 2 = MILLION
+		extraDigits = Math.PI * HMILLION;
+		extraDigits %= 1;
+		extraDigits *= MILLION;
+		System.out.println("extra digits done differently:   " + ("" + extraDigits).replace(".",""));	
+		// 3.141592653589793
+		//           3589792847633362
+		// noticeably different from 
+		// 		   6535897930152714
+				
+		// END Illustration
+		/////////////////////////////////////////////////////////////////////////////////
+		
+		//TODO:
+		//1- Which (if either) extraDigits is accurate?
+		//2- Can we do better with BigDecimal and an algorithm?
+		
+		// All BigDecimal constructors have at least 1 param
+		//BigDecimal bd = new BigDecimal();
+
 		calcFloatPi();
 		//Google says pi =~ 					  3.14159265359
 		System.out.println("Float pi: " + piF); //3.1415968
@@ -28,6 +83,8 @@ public class Pi{
 		//calcDoublePi(); //This won't end anytime soon
 		//System.out.println("Double pi: " + piD);
 		//System.out.println("Double.MAX_VALUE = " + Double.MAX_VALUE);//1.7976931348623157E308
+
+		System.out.println("Done");
 	}
 	
 	//https://en.wikipedia.org/wiki/Pi
