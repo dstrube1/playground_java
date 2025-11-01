790eea1e9139af0afa01bd85bcc8a9a4ac2a215b
David Strube <dstrube3@gatech.edu>
2025-11-01 12:42:23 -0400

    Update Pi.java

--- com/dstrube/Pi.java
+++ com/dstrube/Pi.java
@@ -6,10 +6,13 @@ javac -d bin com/dstrube/Pi.java
 run:
 java -cp bin com.dstrube.Pi
 
-From:
+Relevant:
 https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
-
 https://docs.oracle.com/javase/8/docs/api/java/math/BigDecimal.html
+
+https://en.wikipedia.org/wiki/Pi
+^ around "Rate of convergence"
+	
 
 */
 
@@ -19,11 +22,15 @@ import java.util.HashMap;
 
 
 public class Pi{
+	//A float approximation of pi
 	private static float piF = 4.0f;
+	//A double approximation of pi
 	private static double piD = 4.0;
+	
 	private static long denominator;
-	private static boolean minus;
+	private static boolean isMinus;
 	private static long count;
+	private static final long limit = Long.parseLong("24_000_000_000");
 	
 	// All BigDecimal constructors have at least 1 param, so can't just start off like this:
 	//BigDecimal bd = new BigDecimal();
@@ -43,7 +50,7 @@ public class Pi{
 
 		extraDigits %= 1;
 		extraDigits *= TMILLION;
-		System.out.println("extra digits:    " + ("" + extraDigits).replace(".",""));	
+		//System.out.println("extra digits:    " + ("" + extraDigits).replace(".",""));	
 		// 3.141592653589793
 		// 		   6535897930152714
 		
@@ -67,8 +74,8 @@ public class Pi{
 		extraDigits = Math.PI * HMILLION;
 		extraDigits %= 1;
 		extraDigits *= MILLION;
-		System.out.println("extra digits done differently: " + ("" + extraDigits).replace(".",""));	
-		System.out.println("Compare to Math.Pi:  " + Math.PI);
+		//System.out.println("extra digits done differently: " + ("" + extraDigits).replace(".",""));	
+		//System.out.println("Compare to Math.Pi:  " + Math.PI);
 		// 3.141592653589793
 		//           3589792847633362
 		// noticeably different from 
@@ -92,40 +99,38 @@ public class Pi{
 		
 		calcFloatPi();
 		//Google says pi =~ 					  3.14159265359
-		System.out.println("Float pi: " + piF); //3.1415968
+		//System.out.println("Float pi: " + piF); //3.1415968
 		
-		calcDoublePi_GL(); //This won't end anytime soon
+		//calcDoublePi_GL(); //This won't end anytime soon
 		//System.out.println("Double pi: " + piD);
 		//System.out.println("Double.MAX_VALUE = " + Double.MAX_VALUE);//1.7976931348623157E308
 		
-		//test
+		calcDoublePi_N();
+		
 		// print out precision ends... v here
 		double bigPi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230;
 		//System.out.println("Testing : bigPi: " + bigPi);
 		//What about ...
 		
-		System.out.println("bigPi stored as a BigDecimal: " + bd.toPlainString()); //nice, now we're cooking...
+		//System.out.println("bigPi stored as a BigDecimal: " + bd.toPlainString()); //nice, now we're cooking...
 
 		System.out.println("Done");
 	}
-	
-	//https://en.wikipedia.org/wiki/Pi
-	//around "Rate of convergence"
 	
 	public static void calcFloatPi() {
 		//Gregory–Leibniz series
 		float piB4 = 0.0f;
 		denominator = 1;
-		minus = true;
+		isMinus = true;
 		count = 0;
 		while (piB4 != piF) {
 			piB4 = piF;
 			denominator += 2;
-			if (minus){
-				minus = false;
+			if (isMinus){
+				isMinus = false;
 				piF -= (float)4 / denominator;
 			} else {
-				minus = true;
+				isMinus = true;
 				piF += (float)4 / denominator;
 			}
 			count++;
@@ -133,22 +138,17 @@ public class Pi{
 		System.out.println("Float pi stopped changing after this many cycles: " + count); //16,777,216
 	}
 
-/*
-PROBLEM: this is never true: piB4 != piD
-printing out progress once every 1000000000 shows that at some point there is an overflow or some other error
-that makes pi have an invalid valid, starting with 9.xxx...
-
-SOLUTION: problem was that denominator was an int, and getting incremented led to an overflow before long;
-changing it -and count- to a long made the overflow something that one needn't worry about as soon;
-by the time count is very big (790800000000), the calculation is pretty close (3.1415926535872503)
-note: 1000000000000000000 is a few orders of magnitude bigger than 790800000000
-*/
 	public static void calcDoublePi_GL() {
 		//More Gregory–Leibniz series
 		//Next try Nilakantha...
 		double piB4 = 0.0;
+		
+		//So that this can be called before or after other functions:
+		//Reset piD
+		piD = 4.0;
+		
 		denominator = 1;
-		minus = true;
+		isMinus = true;
 		count = 1;
 		final boolean debug = true;
 		final Map<Long,Double> progressGL = new HashMap<>();
@@ -182,20 +182,30 @@ note: 1000000000000000000 is a few orders of magnitude bigger than 790800000000
 		BigDecimal previousDrift = BigDecimal.ZERO;
 		BigDecimal currentDifference = BigDecimal.ZERO;
 		
+/*
+PROBLEM: this is never true: piB4 != piD
+printing out progress once every 1000000000 shows that at some point there is an overflow or some other error
+that makes pi have an invalid valid, starting with 9.xxx...
+
+SOLUTION: problem was that denominator was an int, and getting incremented led to an overflow before long;
+changing it -and count- to a long made the overflow something that one needn't worry about as soon;
+by the time count is very big (790800000000), the calculation is pretty close (3.1415926535872503)
+note: 1000000000000000000 is a few orders of magnitude bigger than 790800000000
+*/
 		//while (piB4 != piD) {//this comparison is no longer as useful as checking for an overflow
 		while (count > 0){
 			piB4 = piD;
 			denominator += 2;
-			if (minus){
-				minus = false;
+			if (isMinus){
+				isMinus = false;
 				piD -= (double)4 / denominator;
 			} else {
-				minus = true;
+				isMinus = true;
 				piD += (double)4 / denominator;
 			}
 			count++;
 			if (debug && count % 1000000000 == 0){
-				//System.out.println("count: "+count+"; Double pi progress: " + piD);
+				//System.out.println("count: " + count + "; Double pi progress (Gregory–Leibniz series): " + piD);
 				/*
 count: 1000000000; Double pi progress: 3.1415926525880504
 count: 2000000000; Double pi progress: 3.1415926530880767
@@ -222,10 +232,9 @@ count: 22000000000; Double pi progress: 3.1415926535429324
 count: 23000000000; Double pi progress: 3.1415926535449397
 count: 24000000000; Double pi progress: 3.141592653546772
 				*/
-				//TODO:
-				//1- verify that re-running this gives the same numbers (done)
+				//1- verify that re-running this gives the same numbers
 				//2- check each to see how far off they each are, if they're getting closer or farther, or bouncing around
-				//3- see if some other algorithms do better or worse
+				//3- TODO: see if some other algorithms do better or worse
 				if (progressGL.containsKey(count)) {
 					if (progressGL.get(count) != piD){
 						System.out.println("Alert: " + progressGL.get(count) + " != " + piD);
@@ -251,9 +260,9 @@ count: 24000000000; Double pi progress: 3.141592653546772
 							System.out.println("Previous difference was " + previousDrift.toPlainString());
 							final int previousCompareResult = currentDifference.compareTo(previousDrift); 
 							if (previousCompareResult == -1){
-								System.out.println("currentDifference is less than previousDrift");
+								System.out.println("currentDifference is less than previousDrift - getting closer :)");
 							}else if (previousCompareResult == 1){
-								System.out.println("previousDrift is less than currentDifference");
+								System.out.println("previousDrift is less than currentDifference - getting farther :(");
 							}else{
 								//0 - seems a little less unlikely, but who knows by how much
 								System.out.println("previousCompareResult == 0 ?!?!?!?!");
@@ -271,6 +280,69 @@ count: 24000000000; Double pi progress: 3.141592653546772
 			}
 		}
 		System.out.println("Double pi stopped calculating after this many cycles: " + count);
+	}
+	
+	public static void calcDoublePi_N() {
+		//Nilakantha series
+		//converges more rapidly than the Gregory–Leibniz series
+		double piB4 = 0.0;
+		
+		//So that this can be called before or after other functions:
+		//Reset piD
+		piD = 3.0;
+		
+		denominator = 1;
+		long denominatorA = 0;
+		long denominatorB = 1;
+		long denominatorC = 2;
+		isMinus = false;
+		count = 1;
+		final boolean debug = true;
+		while (count <= limit){
+			
+			piB4 = piD;
+			denominatorA += 2;
+			denominatorB += 2;
+			denominatorC += 2;
+			denominator = denominatorA * denominatorB * denominatorC;
+			if (isMinus){
+				isMinus = false;
+				piD -= (double)4 / denominator;
+			} else {
+				isMinus = true;
+				piD += (double)4 / denominator;
+			}
+			count++;
+			if (debug && count % 1000000000 == 0){
+				System.out.println("count: " + count + "; Double pi progress (Nilakantha series): " + piD);
+				/*
+count: 1000000000; Double pi progress (Nilakantha series): 3.141592653656787
+count: 2000000000; Double pi progress (Nilakantha series): 3.141592653623129
+count: 3000000000; Double pi progress (Nilakantha series): 3.141592653636524
+count: 4000000000; Double pi progress (Nilakantha series): 3.1415926535171392
+count: 5000000000; Double pi progress (Nilakantha series): 3.141592654604429
+count: 6000000000; Double pi progress (Nilakantha series): 3.1415926481982037
+count: 7000000000; Double pi progress (Nilakantha series): 3.1415926478103313
+count: 8000000000; Double pi progress (Nilakantha series): 3.1415926467436424
+count: 9000000000; Double pi progress (Nilakantha series): 3.141592646963207
+count: 10000000000; Double pi progress (Nilakantha series): 3.141592646404458
+count: 11000000000; Double pi progress (Nilakantha series): 3.1415926480128578
+count: 12000000000; Double pi progress (Nilakantha series): 3.141592694294119
+count: 13000000000; Double pi progress (Nilakantha series): 3.1415926890000505
+count: 14000000000; Double pi progress (Nilakantha series): 3.141592693834243
+count: 15000000000; Double pi progress (Nilakantha series): 3.1415926947776684
+count: 16000000000; Double pi progress (Nilakantha series): 3.141592694986635
+count: 17000000000; Double pi progress (Nilakantha series): 3.1415926880236005
+count: 18000000000; Double pi progress (Nilakantha series): 3.141592687566944
+count: 19000000000; Double pi progress (Nilakantha series): 3.1415926875863174
+count: 20000000000; Double pi progress (Nilakantha series): 3.1415926867359114
+count: 21000000000; Double pi progress (Nilakantha series): 3.1415926865598167
+count: 22000000000; Double pi progress (Nilakantha series): 3.141592687112181
+count: 23000000000; Double pi progress (Nilakantha series): 3.1415926857157044
+count: 24000000000; Double pi progress (Nilakantha series): 3.1415926847803513
+				*/
+			}
+		}
 	}
 
 }
