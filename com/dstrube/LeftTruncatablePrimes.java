dbe6a91098a914b0de18ed6a42aee09339af2393
David Strube <dstrube3@gatech.edu>
2025-11-05 14:02:11 -0500

    Update LeftTruncatablePrimes.java
    Should be the last edit for a while

--- com/dstrube/LeftTruncatablePrimes.java
+++ com/dstrube/LeftTruncatablePrimes.java
@@ -28,7 +28,7 @@ public class LeftTruncatablePrimes{
 		//According to this:
 		// https://www.youtube.com/watch?v=azL5ehbw_24
 		//this is the largest left truncatable prime:
-		BigInteger largestLeft = new BigInteger("357686312646216567629137"); //357_686_312_646_216_567_629_137 =~ 357 sextillion
+		final BigInteger largestLeft = new BigInteger("357686312646216567629137"); //357_686_312_646_216_567_629_137 =~ 357 sextillion
 		boolean is = largestLeft.isProbablePrime(1);
 		//System.out.println("Is " + largestLeft + " probably prime? " + is); //true
 		
@@ -92,8 +92,35 @@ Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
 		}
 		*/
 		
-		List<Integer> primesList = new ArrayList<>(primes);
-		int lastInList = primesList.get(primes.size() - 1);
+		//Why use any certainty other than 1?
+		//https://stackoverflow.com/questions/27430092/what-is-a-possible-use-case-of-bigintegers-isprobableprime
+		//https://en.wikipedia.org/wiki/Probable_prime
+		//https://en.wikipedia.org/wiki/Pseudoprime
+		//https://mathcrypto.wordpress.com/2014/11/23/large-examples-of-strong-pseudoprimes-to-several-bases/
+		//https://stackoverflow.com/questions/21740745/clarification-of-the-certainty-factor-in-isprobableprime
+		final BigInteger pseudo1 = new BigInteger("1195068768795265792518361315725116351898245581");
+		is = pseudo1.isProbablePrime(1);
+		//System.out.println("Is " + pseudo1 + " probably prime? " + is); //false
+		
+		final BigInteger pseudo2 = new BigInteger("8038374574536394912570796143419421081388376882875581458374889175222974273765333652186502336163960045457915042023603208766569966760987284043965408232928738791850869166857328267761771029389697739470167082304286871099974399765441448453411558724506334092790222752962294149842306881685404326457534018329786111298960644845216191652872597534901");
+		is = pseudo2.isProbablePrime(1);
+		//System.out.println("Is " + pseudo2 + " probably prime? " + is); //false
+		
+		//All these tests are giving what seem to be right answers. At what point does isProbablePrime(1) start to fail?
+		//Would it incorrectly tell us there is a larger LeftTruncatablePrimes than 357686312646216567629137?
+		//This cannot be determined with current methods given the limited range of Sieve.java
+		
+		//At 5:24, the above video says this is the largest right truncatable prime:
+		final BigInteger largestRight = new BigInteger("73939133");
+		//Prove it...
+		
+		//Maybe do this someday:
+		final BigInteger largestLeftRight = new BigInteger("739397");
+		
+		//Not todo: prove that any-deleteable primes are infinite :-p
+		
+		final List<Integer> primesList = new ArrayList<>(primes);
+		final int lastInList = primesList.get(primes.size() - 1);
 		System.out.println("Last int in primes list: " + lastInList); // 2_147_265_203
 		
 		System.out.println("Looking for left truncatable primes in the primes list...");
@@ -113,36 +140,18 @@ Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
 			}
 		}
 		
-		//Why use any certainty other than 1?
-		//https://stackoverflow.com/questions/27430092/what-is-a-possible-use-case-of-bigintegers-isprobableprime
-		//https://en.wikipedia.org/wiki/Probable_prime
-		//https://en.wikipedia.org/wiki/Pseudoprime
-		//https://mathcrypto.wordpress.com/2014/11/23/large-examples-of-strong-pseudoprimes-to-several-bases/
-		//https://stackoverflow.com/questions/21740745/clarification-of-the-certainty-factor-in-isprobableprime
-		BigInteger pseudo1 = new BigInteger("1195068768795265792518361315725116351898245581");
-		is = pseudo1.isProbablePrime(1);
-		//System.out.println("Is " + pseudo1 + " probably prime? " + is); //false
-		
-		BigInteger pseudo2 = new BigInteger("8038374574536394912570796143419421081388376882875581458374889175222974273765333652186502336163960045457915042023603208766569966760987284043965408232928738791850869166857328267761771029389697739470167082304286871099974399765441448453411558724506334092790222752962294149842306881685404326457534018329786111298960644845216191652872597534901");
-		is = pseudo2.isProbablePrime(1);
-		//System.out.println("Is " + pseudo2 + " probably prime? " + is); //false
-		
-		//All these tests are giving what seem to be right answers. At what point does isProbablePrime(1) start to fail?
-		//TODO: Would it incorrectly tell us there is a larger LeftTruncatablePrimes than 357686312646216567629137?
-		
-		//At 5:24, the above video says this is the largest right truncatable prime:
-		BigInteger largestRight = new BigInteger("73939133");
-		//TODO: Prove it with brute force
-		
-		//Maybe todo:
-		BigInteger largestLeftRight = new BigInteger("739397");
-		
-		//Not todo: prove that any-deleteable primes are infinite :-p
+		System.out.println("Looking for right truncatable primes in the primes list...");
+		for (int i = primesList.size() - 1; i > 0; i--){
+			if (isRightTruncatablePrime(primesList.get(i))){
+				System.out.println("Largest right truncatable found: " + primesList.get(i)); //73_939_133 - Proven!
+				break;
+			}
+		}
 		
 		System.out.println("Done");
 	}
 	
-	private static boolean readFile(Path path) {
+	private static boolean readFile(final Path path) {
 		boolean success = false;
 	    Stream<String> lines = null;
 	    try{
@@ -161,7 +170,7 @@ Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
 	    return success;
 	}
 	
-	private static boolean isLeftTruncatablePrime(int candidate){
+	private static boolean isLeftTruncatablePrime(final int candidate){
 		//If this is not in the set of primes, return false
 		if (!primes.contains(candidate)){
 			return false;
@@ -182,7 +191,24 @@ Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
 		}
 		
 		//turn string to a new number
-		int newNumber = Integer.parseInt(numString);
+		final int newNumber = Integer.parseInt(numString);
 		return isLeftTruncatablePrime(newNumber);
+	}
+	
+	private static boolean isRightTruncatablePrime(final int candidate){
+		//If this is not in the set of primes, return false
+		if (!primes.contains(candidate)){
+			return false;
+		}
+		
+		//If this is 1 digit and is in the set of primes, return true
+		if (candidate < 10 && primes.contains(candidate)){
+			return true;
+		}
+		
+		//Trim off last digit
+		final int newNumber = candidate / 10;
+		return isRightTruncatablePrime(newNumber);
+		
 	}
 }
